60485ad7346c39ec49fd7fafd2a23a7edbde1b68
alejandro-aleckmcklaiyre <153336435+alejandro-aleckmcklaiyre@users.noreply.github.com>
2024-12-17 11:45:38 +0800

    Add main.py (#1)
    * Add main.py
    
    * Update the spaces

---
+++ main.py
@@ -0,0 +1,40 @@
+def main_menu():
+    while True:
+        print("============== Main Menu ==============")
+        print("+  [1] Aleck Mcklaiyre Alejandro      +")
+        print("+  [2] Jasmine Robelle De leon        +")
+        print("+  [3] Mikka Kette Esparagoza         +")
+        print("+  [4] Ashley Hermoine Gomez          +")
+        print("+  [5] Loise Nicole Mosenos           +")
+        print("+  [6] Exit                           +")
+        print("=======================================")
+        
+        choice = input("Choose an option: ")
+        
+        if choice == '1':
+            print("-----")
+            # TODO(alejanddro_aleck):
+
+        elif choice == '2':
+            print("-----")
+            # TODO(de_leon_jasmine):
+            
+        elif choice == '3':
+            print("-----")
+            # TODO(esparagoza_mikka):
+
+        elif choice == '4':
+            print("-----")
+            # TODO(gomez_ashley):
+
+        elif choice == '5':
+            print("-----")
+            # TODO(mosenos_loise):
+
+        elif choice == '6':
+            print("\nExiting program.\n")  
+            break
+        else:
+            print("\nInvalid option. Please try again.\n")
+
+main_menu()
